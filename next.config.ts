47c59592f99660947e827fcdc71600da0f217258
Kevin <kdoan@qa.com>
2026-04-06 18:25:56 +0100

    add github pages flow.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  basePath: "/PantryPal", // replace with your exact GitHub repo name
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
